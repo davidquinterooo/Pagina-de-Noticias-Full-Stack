2f39e594c9da97592f07f69a0006861c0bfdd7bc
davidquintero11 <xdavidsimon@gmail.com>
2026-07-19 07:07:40 -0400

    feat: Desarrollo del componente navbar

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxt/image"],
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  modules: ["@pinia/nuxt", "@nuxt/image", "nuxt-bootstrap-icons"],
+  css: ["~/assets/css/main.css", "bootstrap/dist/css/bootstrap.min.css"],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE
-    }
-  }
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
 });
