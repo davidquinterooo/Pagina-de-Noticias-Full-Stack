7b61e3c002bbe2a248642dffbed8519ff2433d11
davidquintero11 <xdavidsimon@gmail.com>
2026-07-21 17:12:13 -0400

    feat: Creacion del componente footer

---
+++ frontend/app/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-fluid bg-light">
+      <footer
+        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4"
+      >
+        <p class="col-md-4 mb-0 text-body-secondary">
+          Name Page
+        </p>
+        <ul class="nav col-md-4 justify-content-end">
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Options 1</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Options 2</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Options 3</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-body-secondary">Options 4</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+</template>
+
+
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="container mt-5">
-    <h1>Bienvenido a Name Page</h1>
-    <p>El contenido de tu página de inicio va aquí.</p>
+  <div class="container">
+    <h1>Name Page</h1>
   </div>
 </template>
